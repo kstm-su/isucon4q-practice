4f3c86a81390626bcfe5f0b42cc9ca6506ed87b0
kstm <kstm-isucon7-ml@shinshu-u.ac.jp>
2017-10-17 05:26:30 +0000

    Update restart sh

--- home/isucon/restart.sh
+++ home/isucon/restart.sh
@@ -1,3 +1,6 @@
+cp /var/log/mysql-slow.sql /var/log.mysql-slow.sql.old
+cp /var/log/nginx/access.log /var/log/nginx/access.log.old
+cp /var/log/nginx/error.log /var/log/nginx/error.log.old
 > /var/log/mysql-slow.sql
 > /var/log/nginx/access.log
 > /var/log/nginx/error.log 
@@ -5,6 +8,11 @@
 service nginx restart
 service mysqld restart
 
-/home/isucon/benchmarker bench
+output=`/home/isucon/benchmarker bench`
 
 mysqldumpslow -s t /var/log/mysql-slow.sql > /home/isucon/log/mysqlslowdump.log
+cat /var/log/nginx/access.log | kataribe -conf /opt/kataribe.toml > /home/isucon/log/kataribe.log
+
+git reset
+git add /home/isucon/log
+git commit -m 'Run benchmark' -m "$output"
